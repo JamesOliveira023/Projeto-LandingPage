33dcedcd746149750bba666de6bc4b6a7b368bd5
JamesOliveira023 <135138774+JamesOliveira023@users.noreply.github.com>
2023-06-06 11:37:25 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var setaDireita = window.document.getElementById("seta-direita")
-var Leonardo = window.document.getElementById("Leonardo")
-var Samanta = window.document.getElementById("Samanta")
-var Bruna = window.document.getElementById("Bruna")
-var setaEsquerda = window.document.getElementById("seta-esquerda")
+var setaDireita = window.document.GetElementById("seta-direita")
+var Leonardo = window.document.GetElementById("Leonardo")
+var Samanta = window.document.GetElementById("Samanta")
+var Bruna = window.document.GetElementById("Bruna")
+var setaEsquerda = window.document.GetElementById("seta-esquerda")
 
 function RolarParaDireita() {
 Leonardo.style = "display:flex"
 Bruna.style = "display:none"
-setaDireita.style = "display:none"
+setaDireita.style = "display:none;"
 setaEsquerda.style = "display:flex; margin-top:55px"
 }
 
